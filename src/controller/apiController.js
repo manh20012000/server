f5f3ead950ab9d24baf1a61ed7dab1f100d69e9a
LÊ VĂN MẠNH <levanmanh20012000@gmail.com>
2023-09-10 21:31:06 +0700

    test

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -2,16 +2,15 @@ import pool from "../config/connectBD.js";
 
 
 let login = async (req, res) => {
+   console.log(req.body.taikhoan, req.body.matkhau)
    try {
       const user = await pool.execute('select taikhoan,matkhau from login where taikhoan=? and matkhau=?', [req.body.taikhoan, req.body.matkhau]);
-
       if (user[0].length > 0) {
          console.log(user[0])
          return res.status(200).json({ data: user[0], msg: "OK", status: 200 })
       } else {
          return res.status(400).json({ msg: "Wrong username or password", status: 400 })
       }
-
    } catch (error) {
       console.log(error)
       return res.status(500).json('loi')
@@ -19,8 +18,9 @@ let login = async (req, res) => {
 
 }
 let Sigin = async (req, res) => {
+         console.log(req.body.taikhoan, req.body.matkhau, req.body.email)
    try {
-      console.log(req.body.taikhoan, req.body.matkhau, req.body.email)
+    
       const user = await pool.execute('insert into login(taikhoan,matkhau,email) value(?,?,?)', [req.body.taikhoan, req.body.matkhau, req.body.email]);
       return res.status(200).json({ data: user[0], msg: "OK", status: 200 })
    }
