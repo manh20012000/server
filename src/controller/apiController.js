d0f5fdd61adfc4b611650eadeb2435d5e2cd32a2
LÊ VĂN MẠNH <levanmanh20012000@gmail.com>
2023-09-17 08:20:16 +0700

    loginconfige

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -14,13 +14,12 @@ let login = async (req, res) => {
       console.log(error)
       return res.status(500).json('loi')
    }
-
 }
 let Sigin = async (req, res) => {
          console.log(req.body.taikhoan, req.body.matkhau, req.body.email)
    try {
     
-      const user = await pool.execute('insert into Login(email,phone,hoten,birth,gender,taikhoan,avata,matkhau,) values(?,?,?,?,?,?,?,?)',
+      const user = await pool.execute('insert into Login(email,phone,hoten,birth,gender,taikhoan,avata,matkhau) values(?,?,?,?,?,?,?,?)',
            [ req.body.email, req.body.phone, req.body.hoten,req.body.birth, req.body.gender,req.body.taikhoan, req.body.avata, req.body.matkhau]);
       return res.status(200).json({ data: user[0], msg: "OK", status: 200 })
    }
