a7f1497fbd1b37d34ab8584b98fe5252bfef78db
LÊ VĂN MẠNH <levanmanh20012000@gmail.com>
2023-09-18 00:05:54 +0700

    mtestmtest

--- src/route/BaivietPost.js
+++ src/route/BaivietPost.js
@@ -5,14 +5,16 @@ import path from "path";
 import appRoot from 'app-root-path';
 import { uuid } from "uuidv4";
 const baiviet = Router();
+
 baiviet.post('/tao_bai_viet', async function (req, res, next) { 
     const {  isText, datePost,idLogin } = req.body
-    console.log(idLogin, isText, datePost)
+    console.log( idLogin, isText, datePost)
     const postUpLoad = await pool.execute(`insert into baiviet(trangThai,datePost,idLogin) values(?,?,?)`, [isText, datePost, idLogin]);
     console.log(postUpLoad)
     return res.status(200).json({ data: postUpLoad[0], msg: "OK", status: 200 })
 })
-//thuc hiện post ảnh lên server
+
+// thuc hiện post ảnh lên server
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null,'public/uploads/')
@@ -29,21 +31,21 @@ const storage = multer.diskStorage({
     }
     cb(null, true);
 };
+//băt đau thuc hiẹn post
+// baiviet.post('/file', upload.array('uploaded_file', 12), async function (req, res, next) {
 
-baiviet.post('/file', upload.array('uploaded_file', 12), async function (req, res, next) {
-
-    let files = req.files
-    console.log(files)
-    const fileUrls = files.map(file => {
-          return '/uploads/'+file.filename // cộng 2 cái này bằng cái đường dẫn path 
-    })
-    fileUrls.forEach(async url => {
-      const query= await pool.execute(`insert into file(linkFile) values('${url}')`)
-     });
-baiviet.post('/postImage',upload.array('uploaded_file', 12),async function (req, res, next) { 
-    const { idLogin,isText,datePost } = req.body
-    const postUpLoad = await pool.execute(`insert into baiviet(status,datePost,idLogin) values(?,?,?)`, [isText,datePost,idLogin]),
+//     let files = req.files
+//     console.log(files)
+//     const fileUrls = files.map(file => {
+//           return '/uploads/'+file.filename // cộng 2 cái này bằng cái đường dẫn path 
+//     })
+//     fileUrls.forEach(async url => {
+//       const query= await pool.execute(`insert into file(linkFile) values('${url}')`)
+//      });
+// baiviet.post('/postImage',upload.array('uploaded_file', 12),async function (req, res, next) { 
+//     const { idLogin,isText,datePost } = req.body
+//     const postUpLoad = await pool.execute(`insert into baiviet(status,datePost,idLogin) values(?,?,?)`, [isText,datePost,idLogin]),
      
 
-})
+//})
 export default baiviet;
