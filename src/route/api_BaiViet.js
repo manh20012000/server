07cdc9a39a091eff8ddc6299201aec97f083400e
LÊ VĂN MẠNH <levanmanh20012000@gmail.com>
2023-09-26 16:38:19 +0700

    mchnger uuid

--- src/route/api_BaiViet.js
+++ src/route/api_BaiViet.js
@@ -1,100 +1,80 @@
-import{ Router, query } from "express";
+import { Router, query } from "express";
 import multer from "multer";
 import pool from "../config/connectBD.js";
 import path from "path";
-import appRoot from 'app-root-path';
-import { uuid } from "uuidv4";
+import appRoot from "app-root-path";
+import uuid from "react-uuid";
 const baiviet = Router();
 //thuc hiện post ảnh lên server
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null,'public/uploads/')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + uuid().substring(0,8) + path.extname(file.originalname));
-    }
-  })
-  const imageFilter = function (req, file, cb) {
-    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
-      req.fileValidationError = 'Only image files are allowed!';
-      return cb(new Error('Only image files are allowed!'), false);
-    }
-    cb(null, true);
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,file.fieldname + "-" +uuid().substring(0, 8) +path.extname(file.originalname)      
+    );
+  },
+});
+const imageFilter = function (req, file, cb) {
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+    req.fileValidationError = "Only image files are allowed!";
+    return cb(new Error("Only image files are allowed!"), false);
+  }
+  cb(null, true);
 };
 //băt đau thuc hiẹn post file
-const upload = multer({ storage: storage, fileFilter: imageFilter, })
-console.log(upload+'upload')
-baiviet.post('/filesPost', upload.array('ArayImages', 12), async function (req, res, next) {
-  const { trangThai,
-    datePost,
-    idLogin,
-    feel,
-    permission,
-    vitri,
-    ArayImages
-  } = req.body;
-  console.log(datePost, idLogin, feel, permission, vitri, ArayImages);
-console.log(JSON.stringify(upload)+'thu mục uload')
-  const Image = [];
+const upload = multer({ storage: storage, fileFilter: imageFilter });
+console.log(upload + "upload");
+baiviet.post("/filesPost",upload.array("ArayImages", 12), async function (req, res, next) {
+    const {trangThai, datePost,   feel, permission,vitri, ArayImages} = req.body;
+    console.log(datePost, idLogin, feel, permission, vitri, ArayImages);
+    console.log(JSON.stringify(upload) + "thu mục uload");
+    const Image = [];
     try {
-      const files = req.files;
-      console.log(files + 'là ');
-      const fileUrl = files.map(file => {
-      Image.push('/uploads' + file.fieldname);
-        //  return '/uploads' + file.fieldname;
-        console.log('trả về Image'+Image)
-        })
-        // fileUrl.forEach( async  url => {
-        //     const query = await pool.execute('insert into listImage(image,timePost,idLogin) values(?,?,?)', [url, datePost, idLogin])
-        //      return res.json('Capnhat thanh cong')
-        // });
-     } catch (err) {
+
+      console.log( req.files + "là ");
+      const fileUrl = req.files.map((file) => {
+        Image.push("/uploads" + file.filename);
+        console.log("trả về Image" + Image);
+        return "/uploads" + file.fieldname;
+      });
+      // fileUrl.forEach( async  url => {
+      //     const query = await pool.execute('insert into listImage(image,timePost,idLogin) values(?,?,?)', [url, datePost, idLogin])
+      //      return res.json('Capnhat thanh cong')
+      // });
+    } catch (err) {
       return res.status(500);
     }
-})
-export default baiviet
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+  }
+);
+export default baiviet;
 
 // baiviet.post('/file', upload.array('uploaded_file', 12), async function (req, res, next) {
 
 //     let files = req.files
 //     console.log(files)
 //     const fileUrls = files.map(file => {
-//           return '/uploads/'+file.filename // cộng 2 cái này bằng cái đường dẫn path 
+//           return '/uploads/'+file.filename // cộng 2 cái này bằng cái đường dẫn path
 //     })
 //     fileUrls.forEach(async url => {
 //       const query= await pool.execute(`insert into file(linkFile) values('${url}')`)
 //      });
-// baiviet.post('/postImage',upload.array('uploaded_file', 12),async function (req, res, next) { 
+// baiviet.post('/postImage',upload.array('uploaded_file', 12),async function (req, res, next) {
 //     const { idLogin,isText,datePost } = req.body
 //     const postUpLoad = await pool.execute(`insert into baiviet(status,datePost,idLogin) values(?,?,?)`, [isText,datePost,idLogin]),
-     
 
 // })
 // ;baiviet.get('/getBaiViet', async function (req, res, next) {
 //     try {
 //         const query = await pool.execute('select email,hoten,birth,gender,avata,baiviet.datePost,trangThai,permission,feel,idlistImage,image,Location' +' from Login,baiviet,listImage where Login.idLogin=baiviet.idLogin '
-//         + 'and listImage.datePost=baiviet.datePost' ) 
-      
+//         + 'and listImage.datePost=baiviet.datePost' )
+
 //     } catch (err) {
 //         res.json(err);
 //        }
 //  })
-// baiviet.post('/tao_bai_viet', async function (req, res, next) { 
+// baiviet.post('/tao_bai_viet', async function (req, res, next) {
 //     try {
 //         const { trangThai, datePost, idLogin, permission, feel, vitri} = req.body
 //          console.log(trangThai, datePost, idLogin, permission, feel, vitri)
