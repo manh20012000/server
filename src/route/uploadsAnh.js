04dcc95ab19d13d0d8ced6c7a312ecc1b4825886
LÊ VĂN MẠNH <levanmanh20012000@gmail.com>
2023-09-27 12:48:02 +0700

    cganger

--- src/route/uploadsAnh.js
+++ src/route/uploadsAnh.js
@@ -5,7 +5,7 @@ import path from "path";
 import appRoot from "app-root-path";
 import uuid from "react-uuid";
 const uploadanh=Router();
-const upload = multer({ dest: 'public/uploads/' });
+const uploadanh = multer({ dest: 'public/uploads/' });
 uploadanh.post('/uploadAnh', upload.array('ArayImages', 12), async (req, res) => {
     console.log(JSON.stringify(req.files))
     const Image = [];
