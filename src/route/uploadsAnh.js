437f10b9545ae424cd01d2f4069e67535bdfe5aa
LÊ VĂN MẠNH <levanmanh20012000@gmail.com>
2023-09-27 14:36:07 +0700

    hâhhâh

--- src/route/uploadsAnh.js
+++ src/route/uploadsAnh.js
@@ -4,14 +4,26 @@ import pool from "../config/connectBD.js";
 import path from "path";
 import appRoot from "app-root-path";
 import uuid from "react-uuid";
-const uploadanh=Router();
-const uploads = multer({ dest: 'public/uploads/' });
+const uploadanh = Router();
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      // console.log(req.body+'destination')
+      cb(null, "/public/uploads/");
+    },
+    filename: function (req, file, cb) {
+      // console.log(req.body+'filenam')
+      cb(
+        null,file.filename + "-" +uuid().substring(0, 8) +path.extname(file.originalname)      
+      );
+    },
+});
+const uploads = multer({ storage: storage });
 uploadanh.post('/uploadAnh', uploads.array('ArayImages', 12), async (req, res) => {
     console.log(JSON.stringify(req.files))
     const Image = [];
     try {
         const fileUrl = await req.files.map((file) => {
-            Image.push("/uploads" + file.filename);
+            Image.push("/uploads/" + file.filename);
             console.log("trả về Image" + Image);
             return "/uploads/" + file.filename;
         }
