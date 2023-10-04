521a77b30ac4a383d910717cb61dc954c39bf957
LÊ VĂN MẠNH <levanmanh20012000@gmail.com>
2023-10-04 08:53:24 +0700

    changeupload

--- src/route/uploadsAnh.js
+++ src/route/uploadsAnh.js
@@ -8,59 +8,69 @@ import uuid from "react-uuid";
 import baiviet from "../model/baiviet.js";
 const uploadAnh = Router();
 const storage = multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null,'public/uploads/')
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.fieldname + '-' + uuid().substring(0,8) + path.extname(file.originalname));
-    }
-  })
-  const imageFilter = function (req, file, cb) {
-    if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
-      req.fileValidationError = 'Only image files are allowed!';
-      return cb(new Error('Only image files are allowed!'), false);
-    }
-    cb(null, true);
-  };
-const uploads = multer({ storage: storage,imageFilter:imageFilter});
+  destination: function (req, file, cb) {
+    cb(null, "public/uploads/");
+  },
+  filename: function (req, file, cb) {
+    cb(
+      null,
+      file.fieldname +
+        "-" +
+        uuid().substring(0, 8) +
+        path.extname(file.originalname)
+    );
+  },
+});
+const imageFilter = function (req, file, cb) {
+  if (!file.originalname.match(/\.(jpg|JPG|jpeg|JPEG|png|PNG|gif|GIF)$/)) {
+    req.fileValidationError = "Only image files are allowed!";
+    return cb(new Error("Only image files are allowed!"), false);
+  }
+  cb(null, true);
+};
+const uploads = multer({ storage: storage, imageFilter: imageFilter });
 // const uploads = multer({ dest:'public/uploads/' });
 
-uploadAnh.post('/uploadAnh', uploads.array('ArayImages', 12), async (req, res) => {
-  let info = {
-    protocol: req.protocol,
-    host: req.get('host')
-  }
-  console.log(JSON.stringify(req.files)+'anh duo tra')
-  const imagePaths = await req.files.map((file) => {
-    console.log("trả về Image->" + file.filename);
-    let link 
-        return `${info.protocol}://${info.host}`+"/uploads/" + file.filename;
-      }); 
-  console.log(imagePaths)
-          const initialLike = {
-            User: req.body.idLogin,
-            Trangthai: false
-          };
-                const newPost ={ 
-                Trangthai: req.body.trangThai,
-                DatePost: req.body.datePost,    
-                Fell: req.body.feel,
-                Pemission: req.body.permission,
-                Loaction: req.body.vitri,
-                User: req.body.idLogin, 
-                Image: imagePaths      
-            };
-  try {
-    const createdPost = await Baiviet.create(newPost);
-    createdPost.Like.push(initialLike);
-    console.log('vao luwu vao database' + newPost)
-    const data = await createdPost.save();
-    console.log('xuong đay')
-    return res.status(200).json({ msg: "OK", status: 200 });
-  } catch (err) {
-    console.log(err)
-        return res.status(501).json(err);
+uploadAnh.post(
+  "/uploadAnh",
+  uploads.array("ArayImages", 12),
+  async (req, res) => {
+    let info = {
+      protocol: req.protocol,
+      host: req.get("host"),
+    };
+    console.log(JSON.stringify(req.files) + "anh duo tra");
+    const imagePaths = await req.files.map((file) => {
+      console.log("trả về Image->" + file.filename);
+      let link;
+      return `${info.protocol}://${info.host}` + "/uploads/" + file.filename;
+    });
+    console.log(imagePaths);
+   
+    const newPost = {
+      Trangthai: req.body.trangThai,
+      DatePost: req.body.datePost,
+      Fell: req.body.feel,
+      Pemission: req.body.permission,
+      Loaction: req.body.vitri,
+      User: req.body.idLogin,
+      Like: [
+        {
+          User: req.body.idLogin,
+          Trangthai: false
+        }
+      ],
+      Image: imagePaths,
+    };
+    try {
+      const data = await new baiviet(newPost).save();
+      console.log("vao luwu vao database" + newPost);
+      console.log("xuong đay");
+      return res.status(200).json({ msg: "OK", status: 200 });
+    } catch (err) {
+      console.log(err);
+      return res.status(501).json(err);
     }
   }
-)
+);
 export default uploadAnh;
